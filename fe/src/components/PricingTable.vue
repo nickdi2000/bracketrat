af78cc4306fc920a9f1b56df4264052ed3297f58
nick di <nickdifelice@gmail.com>
2024-07-18 09:03:52 -0400

    udpate pricing

--- fe/src/components/PricingTable.vue
+++ fe/src/components/PricingTable.vue
@@ -5,7 +5,7 @@
       class="relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
     >
       <div
-        class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12"
+        class="max-w-xl mb-10 mx-auto md:mx-auto text-center lg:max-w-2xl md:mb-12"
       >
         <h2
           class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto"
@@ -33,7 +33,11 @@
                 height="24"
               ></rect>
             </svg>
-            <span class="relative sm:text-white lg:text-gray-800">Pricing</span>
+            <div
+              class="relative text-white md:text-gray-200 flex flex-row justify-center lg:text-gray-800"
+            >
+              Pricing
+            </div>
           </span>
         </h2>
         <div class="text-orange-500 text-lg uppercase font-bold">
